c345e3a4d2192f625ba0bc128bba73e0472ba3fc
digital_snrt <khassal.mohamed1@gmail.com>
2024-06-29 18:16:02 +0100

    add class theme for dark and white

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_app/fooderMo_theme.dart';
 
 void main() {
   runApp(const FooderMo());
@@ -12,13 +12,25 @@ class FooderMo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // todo: create theme
+    final theme = FooderMoTheme.light();
     //todo; apply home widget
 
     return MaterialApp(
+        theme: theme,
         title: 'FooderMo',
         home: Scaffold(
-            appBar: AppBar(
-          title: Text('Flutter Demo Home Page'),
-        ),body: const Center(child: Text('Let\'s get cooking '),),));
+          appBar: AppBar(
+            title: Text(
+              'Flutter Demo Home Page',
+              style: theme.textTheme.bodySmall,
+            ),
+          ),
+          body:  Center(
+            child: Text(
+              'Let\'s get cooking ',
+              style: theme.textTheme.headlineLarge
+            ),
+          ),
+        ));
   }
 }
