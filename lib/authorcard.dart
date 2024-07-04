7dffe6c1b057d0463a16185479c6fd3b995be909
digital_snrt <khassal.mohamed1@gmail.com>
2024-07-04 21:31:40 +0100

    finish with understanding widget

--- lib/authorcard.dart
+++ lib/authorcard.dart
@@ -4,7 +4,7 @@ import 'package:flutter_app/circle_image.dart';
 import 'package:flutter_app/fooderMo_theme.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class Authorcard extends StatelessWidget {
+class Authorcard extends StatefulWidget {
   final String authorname;
   final String title;
   final ImageProvider? imageProvider;
@@ -16,40 +16,55 @@ class Authorcard extends StatelessWidget {
       this.imageProvider});
 
   @override
+  State<Authorcard> createState() => _AuthorcardState();
+}
+
+class _AuthorcardState extends State<Authorcard> {
+  bool _isFavorite = false;
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(16).w,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
         children: [
-
           Row(
             children: [
               CircleImage(
-                imageProvider: imageProvider,
+                imageProvider: widget.imageProvider,
                 imageRadius: 28.r,
               ),
               SizedBox(
                 width: 8.w,
               ),
-
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(authorname,style: FooderMoTheme.lightTextTheme.headlineMedium,),
-                  Text(title,style: FooderMoTheme.lightTextTheme.headlineSmall,)
-
+                  Text(
+                    widget.authorname,
+                    style: FooderMoTheme.lightTextTheme.headlineMedium,
+                  ),
+                  Text(
+                    widget.title,
+                    style: FooderMoTheme.lightTextTheme.headlineSmall,
+                  )
                 ],
-              ),],
+              ),
+            ],
           ),
-
-          IconButton(onPressed: (){
-            const snackBar =SnackBar(content: Text('Favorite Pressed'));
-            ScaffoldMessenger.of(context).showSnackBar(snackBar);
-
-          }, icon: const Icon(Icons.favorite_border),iconSize: 30,color: Colors.grey[400],),
-
+          IconButton(
+            onPressed: () {
+              const  snackBar = SnackBar(content: Text('Favorite Pressed'));
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              setState(() {
+                _isFavorite=!_isFavorite;
+              });
+            },
+            icon:  Icon(_isFavorite ? Icons.favorite:Icons.favorite_border),
+            iconSize: 30,
+            color: Colors.redAccent,
+          ),
         ],
       ),
     );
--- lib/card3.dart
+++ lib/card3.dart
@@ -1,4 +1,6 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/fooderMo_theme.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Card3 extends StatelessWidget {
@@ -17,13 +19,146 @@ class Card3 extends StatelessWidget {
           children: [
             Container(
               decoration: BoxDecoration(
-                color: Colors.black.withOpacity(.6), 
-              borderRadius: BorderRadius.all(Radius.circular(16))
+                  color: Colors.black.withOpacity(.6),
+                  borderRadius: BorderRadius.all(Radius.circular(16))),
+            ), // TODO ADD COLUMN TEXT AND ICON
+            Container(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(
+                    Icons.book,
+                    color: Colors.white,
+                    size: 40.sp,
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    'Recipe Trends',
+                    style: FooderMoTheme.darkTextTheme.headlineMedium,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  )
+                ],
               ),
-              // TODO ADD COLUMN TEXT AND ICONE
-              // TODO ADD CHIPS IN CENTER
-
             ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Wrap(
+                  alignment: WrapAlignment.start,
+                  spacing: 12,
+                  runSpacing: 12,
+                  children: [
+                    Chip(
+                        label: Text(
+                          'Healthy',
+                          style: FooderMoTheme.darkTextTheme.bodyLarge,
+                        ),
+                        backgroundColor: Colors.black.withOpacity(0.7),
+                        onDeleted: () {
+                          log('delete');
+                        },
+                        side: BorderSide.none,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              20.0.r), // Adjust the radius as needed
+                          // This removes the white border
+                        )),
+                    Chip(
+                        label: Text(
+                          'Vegan',
+                          style: FooderMoTheme.darkTextTheme.bodyLarge,
+                        ),
+                        backgroundColor: Colors.black.withOpacity(0.7),
+                        onDeleted: () {
+                          log('delete');
+                        },
+                        side: BorderSide.none,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              20.0.r), // Adjust the radius as needed
+                          // This removes the white border
+                        )),
+                    Chip(
+                        label: Text(
+                          'Carrot',
+                          style: FooderMoTheme.darkTextTheme.bodyLarge,
+                        ),
+                        backgroundColor: Colors.black.withOpacity(0.7),
+                        side: BorderSide.none,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              20.0.r), // Adjust the radius as needed
+                          // This removes the white border
+                        )),
+                    Chip(
+                        label: Text(
+                          'Greens',
+                          style: FooderMoTheme.darkTextTheme.bodyLarge,
+                        ),
+                        backgroundColor: Colors.black.withOpacity(0.7),
+                        side: BorderSide.none,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              20.0.r), // Adjust the radius as needed
+                          // This removes the white border
+                        )),
+                    Chip(
+                        label: Text(
+                          'Wheat',
+                          style: FooderMoTheme.darkTextTheme.bodyLarge,
+                        ),
+                        backgroundColor: Colors.black.withOpacity(0.7),
+                        side: BorderSide.none,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              20.0.r), // Adjust the radius as needed
+                          // This removes the white border
+                        )),
+                    Chip(
+                        label: Text(
+                          'Pescetarian',
+                          style: FooderMoTheme.darkTextTheme.bodyLarge,
+                        ),
+                        backgroundColor: Colors.black.withOpacity(0.7),
+                        side: BorderSide.none,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              20.0.r), // Adjust the radius as needed
+                          // This removes the white border
+                        )),
+                    Chip(
+                        label: Text(
+                          'Mint',
+                          style: FooderMoTheme.darkTextTheme.bodyLarge,
+                        ),
+                        backgroundColor: Colors.black.withOpacity(0.7),
+                        side: BorderSide.none,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              20.0.r), // Adjust the radius as needed
+                          // This removes the white border
+                        )),
+                    Chip(
+                        label: Text(
+                          'Lemongrass',
+                          style: FooderMoTheme.darkTextTheme.bodyLarge,
+                        ),
+                        backgroundColor: Colors.black.withOpacity(0.7),
+                        side: BorderSide.none,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              20.0.r), // Adjust the radius as needed
+                          // This removes the white border
+                        ))
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
